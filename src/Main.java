4bf69bc63dae7048b52ec666871b810d478cb32d
Hugo Ortiz <personals_ervices@hotmail.com>
2025-06-09 00:44:17 -0600

    Crea Repositorio y Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        teclado.useLocale(Locale.US);
+
+        
+    }
+}
